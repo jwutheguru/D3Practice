0543f5359cdef2618f7e9fdf16113fd49cc92f44
Jianzhuo Wu <jwutheguru@gmail.com>
2016-03-24 23:58:09 -0700

    Initial commit 2

--- js/graph.js
+++ js/graph.js
@@ -1,33 +1,41 @@
 $(function() {
 
-    var data = [4, 8, 15, 16, 23, 42];
-
     var width = 420;
     var barHeight = 20;
 
     var x = d3.scale.linear()
-        .domain([0, d3.max(data)])
-        .range([0, 420]);
+        .range([0, width]);
 
     var chart = d3.select(".chart")
-        .attr("width", width)
-        .attr("height", barHeight * data.length);
+        .attr("width", width);
 
-    var bar = chart.selectAll("g")
-        .data(data)
-        .enter().append("g")
-        .attr("transform", function(d, i) {
-            return "translate(0," + i * barHeight + ")";
-        });
+    d3.csv("data/data.csv", // http://jwu.codes/stash/data.tsv
+        function(d) {
+            d.value = +d.value;
+            return d;
+        }, 
+        function(error, data) {
+            x.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-    bar.append("rect")
-        .attr("width", x)
-        .attr("height", barHeight - 1);
+            chart.attr("height", barHeight * data.length);
 
-    bar.append("text")
-        .attr("x", function(d) { return x(d) - 3; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function(d) { return d; });
+            var bar = chart.selectAll("g")
+                .data(data)
+                .enter().append("g")
+                .attr("transform", function(d, i) {
+                    return "translate(0, " + i * barHeight + ")";
+                });
+
+            bar.append("rect")
+                .attr("width", function(d) { return x(d.value); })
+                .attr("height", barHeight - 1);
+
+            bar.append("text")
+                .attr("x", function(d) { return x(d.value) - 3; })
+                .attr("y", barHeight / 2)
+                .attr("dy", ".35em")
+                .text(function(d) { return d.value; });
+        }
+    );
 
 });
