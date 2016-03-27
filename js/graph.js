53b14a169258c3c469be0f942d9b7a72c674ed64
Jianzhuo Wu <jwutheguru@gmail.com>
2016-03-26 17:28:29 -0700

    Update tutorial follow-along.

--- js/graph.js
+++ js/graph.js
@@ -1,41 +1,42 @@
 $(function() {
 
-    var width = 420;
-    var barHeight = 20;
+    var width = 960;
+    var height = 500;
 
-    var x = d3.scale.linear()
-        .range([0, width]);
+    var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear().range([height, 0]);
 
     var chart = d3.select(".chart")
-        .attr("width", width);
+        .attr("width", width)
+        .attr("height", height);
 
-    d3.csv("data/data.csv", // http://jwu.codes/stash/data.tsv
-        function(d) {
-            d.value = +d.value;
-            return d;
-        }, 
-        function(error, data) {
-            x.domain([0, d3.max(data, function(d) { return d.value; })]);
+    d3.csv("data/data.csv", function(d) {
+        d.value = +d.value;
+        return d;
+    }, function(error, data) {
+        x.domain(data.map(function(d) { return d.name; }));
+        y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-            chart.attr("height", barHeight * data.length);
+        var bar = chart.selectAll("g")
+            .data(data)
+            .enter().append("g")
+            .attr("transform", function(d, i) { 
+                return "translate(" + x(d.name) + ",0)";
+            });
 
-            var bar = chart.selectAll("g")
-                .data(data)
-                .enter().append("g")
-                .attr("transform", function(d, i) {
-                    return "translate(0, " + i * barHeight + ")";
-                });
+        bar.append("rect")
+            .attr("y", function(d) { return y(d.value); })
+            .attr("height", function(d) { return height - y(d.value); })
+            .attr("width", x.rangeBand());
 
-            bar.append("rect")
-                .attr("width", function(d) { return x(d.value); })
-                .attr("height", barHeight - 1);
+        bar.append("text")
+            .attr("x", x.rangeBand() / 2)
+            .attr("y", function(d) { return y(d.value) + 3; })
+            .attr("dy", ".75em")
+            .text(function(d) { return d.value; });
 
-            bar.append("text")
-                .attr("x", function(d) { return x(d.value) - 3; })
-                .attr("y", barHeight / 2)
-                .attr("dy", ".35em")
-                .text(function(d) { return d.value; });
-        }
-    );
+
+    });
 
 });
